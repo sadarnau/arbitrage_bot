87a15477af1156ae22935beca24ad60160a2a837
samphoto <samidarnaud@gmail.com>
2022-02-23 18:38:41 +0100

    brownie config file

--- brownie_version/tests/conftest.py
+++ brownie_version/tests/conftest.py
@@ -1,13 +1,24 @@
 import pytest
+import abi
 
 
 @pytest.fixture(autouse=True)
 def setup(fn_isolation):
-    """
-    Isolation setup fixture.
-    This ensures that each test runs against the same base environment.
-    """
     pass
+
+
+@pytest.fixture
+def aave_lending_pool(Contract):
+    test = Contract("0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5")
+    print(test)
+    # Contract.from_abi(
+    # "aave_lending_pool", "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5", abi.aave_lending)
+    # yield Contract("0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5")
+
+
+@pytest.fixture
+def flashContract(FlashGordon, aave_lending_pool, accounts):
+    yield FlashGordon.deploy(aave_lending_pool, {"from": accounts[0]})
 
 
 # @pytest.fixture(scope="module")
--- brownie_version/tests/test_flashloan.py
+++ brownie_version/tests/test_flashloan.py
@@ -1,10 +1,8 @@
-from brownie import network
+def test_eth_flashloan(accounts, flashContract):
+    print(flashContract.address)
+    # FlashGordon.deploy("0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5", {
+    #                    "from": accounts[0]})
 
-
-def test_eth_flashloan(accounts, FlashGordon):
-    print(accounts[0].balance())
-    print(accounts[0].address)
-    FlashGordon.deploy("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", {
-                       "from": accounts[0]})
-    # accounts[0].transfer(WETH, "2 ether", network.priority_fee("auto"))
+    accounts[0].transfer(
+        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", "2 ether")
     # WETH.transfer(FlashGordon, "2 ether", {"from": accounts[0]})
